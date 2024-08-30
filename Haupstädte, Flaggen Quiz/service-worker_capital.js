a4c53de365d13d36e0357bdfcb90f6b88935f36d
Sebastian-H-G <sebastianhermes@icloud.com>
2024-08-30 15:48:19 +0200

    u

--- Haupstädte, Flaggen Quiz/service-worker_capital.js
+++ Haupstädte, Flaggen Quiz/service-worker_capital.js
@@ -1,33 +1,47 @@
 const CACHE_NAME = 'capital-quiz-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
+  '/All_Capitals.html',
   '/capitals.css',
   '/all_capitals.js',
-  '/Logos/Capital.ico',
-  '/Logos/Capital-16x16.webp',
-  '/Logos/Capital-32x32.webp',
-  '/Logos/Capital-48x48.webp',
-  '/Logos/Capital.webp',
-  '/Logos/Capital-57x57.webp',
   '/Logos/Capital-72x72.webp',
   '/Logos/Capital-114x114.webp',
-  '/Logos/Capital-152x152.webp',
-  '/Logos/Capital-180x180.webp'
+  '/Logos/Capital-180x180.webp',
+  '/Logos/Capital-192x192.webp',
+  '/Logos/Capital-512x512.webp'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
