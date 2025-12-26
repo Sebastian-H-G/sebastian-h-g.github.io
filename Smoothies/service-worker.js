e6877056e36ce7e230c7badc077ba1530c083e8d
Sebastian-H-G <sebastianhermes@icloud.com>
2025-12-26 10:47:39 -0600

    update

---
+++ Smoothies/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "smoothie-lab-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon.png",
+  "./icons/icon-16.png",
+  "./icons/icon-32.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
